a417251f661cdb63b9df1a323519663a0eb45fd8
Debasishpp <debasishpanda888@gmail.com>
2023-06-02 15:04:20 +0530

    4th

--- JenkinsProject/src/test/java/Demo/Myp1Test.java
+++ JenkinsProject/src/test/java/Demo/Myp1Test.java
@@ -1,11 +1,30 @@
 package Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Myp1Test {
+	WebDriver driver;
+	
 @Test
 	public void m1()
 	{
-		System.out.println("hi");
+	
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+		}else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}else {
+			driver=new ChromeDriver();
+		}
+		
+		driver.get(URL);
 	}
 }
