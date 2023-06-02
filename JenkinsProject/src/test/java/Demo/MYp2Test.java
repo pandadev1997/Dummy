d0a18b750a2e6935c13b9295ef8a79a9983a2b59
Debasishpp <debasishpanda888@gmail.com>
2023-06-02 11:20:12 +0530

    1

---
+++ JenkinsProject/src/test/java/Demo/MYp2Test.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class MYp2Test {
+@Test
+	public void m2()
+	{
+		System.out.println("hello");
+	}
+}
---
+++ JenkinsProject/src/test/java/Demo/Myp1Test.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Myp1Test {
+@Test
+	public void m1()
+	{
+		System.out.println("hi");
+	}
+}
